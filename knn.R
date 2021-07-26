250cb8932a6f3d7fd802194e214384152c284a0b
CaptainNemo <rohitashwachaks@gmail.com>
2021-07-26 15:00:56 -0500

    Updated knn and replotted graphs

--- knn.R
+++ knn.R
@@ -12,6 +12,9 @@ library(caret)
 # install.packages("scatterplot3d") # Install
 library("scatterplot3d")
 
+# install.packages('smotefamily', dependencies = TRUE)
+library(smotefamily)
+library(ipred)
 
 set.seed(0)
 
@@ -41,52 +44,49 @@ data <- data[!is.na(data$bmi),] # Drop BMI = N/A
 n <- dim(data)[1]
 
 #----SMOTE(OverSampling)----
-# install.packages('smotefamily', dependencies = TRUE)
-library(smotefamily)
-library(ipred)
 
-Union_data <- data[,c('age','avg_glucose_level','bmi','stroke')]
-# Union_data <- Union_data[complete.cases(Union_data),]
-
-#Using SMOTE to oversample and create balanced classes
-Balanced_data <- SMOTE(X = Union_data[,-ncol(Union_data)],target = Union_data$stroke, K=4, dup_size = 20)
-
-df <- Balanced_data$data
-df$stroke <- as.factor(df$class)
-table(df$stroke)
-n <- dim(df)[1]
-
-plot(col = df$stroke, pch=43, cex=1,
-     main = "Sample class distribution",
-     df %>% select(age, bmi, avg_glucose_level))
-
-# --Age v/s Glucose
-plot(df$age, df$avg_glucose_level,
-     col = df$stroke, pch=43, cex=1,
-     xlab = "Age",
-     ylab = "Average Glucose level",
-     main = "Oversampled Data Distribution\n(Age v/s Glucose)")
-legend(1, 275, legend=c("No Stroke", "Stroke"), col=c("black", "red"), lty=1:2, cex=0.5)
-
-# --BMI v/s Glucose
-plot(df$bmi, df$avg_glucose_level,
-     col = df$stroke, pch=43, cex=1,
-     xlab = "BMI",
-     ylab = "Average Glucose level",
-     main = "Oversampled Data Distribution\n(BMI v/s Glucose)")
-legend(80, 275, legend=c("No Stroke", "Stroke"), col=c("black", "red"), lty=1:2, cex=0.5)
-
-
-# --Plotting All 3 continuous Variables
-color <- c("#E69F00", "#56B4E9")
-shape <- c(42,43)
-scatterplot3d(x = df$age, xlab = "Age",
-              z = df$avg_glucose_level, zlab = "Avg. Glucose Level",
-              y = df$bmi, ylab = "BMI",
-              main = "Oversampled Stroke DataSet",
-              pch = shape[as.numeric(df$stroke)], 
-              color = color[as.numeric(df$stroke)],
-              box = FALSE, angle = 20)
+# Union_data <- data[,c('age','avg_glucose_level','bmi','stroke')]
+# # Union_data <- Union_data[complete.cases(Union_data),]
+# 
+# #Using SMOTE to oversample and create balanced classes
+# Balanced_data <- SMOTE(X = Union_data[,-ncol(Union_data)],target = Union_data$stroke, K=4, dup_size = 20)
+# 
+# df <- Balanced_data$data
+# df$stroke <- as.factor(df$class)
+# table(df$stroke)
+# n <- dim(df)[1]
+# 
+# plot(col = df$stroke, pch=43, cex=1,
+#      main = "Sample class distribution",
+#      df %>% select(age, bmi, avg_glucose_level))
+# 
+# # --Age v/s Glucose
+# plot(df$age, df$avg_glucose_level,
+#      col = df$stroke, pch=43, cex=1,
+#      xlab = "Age",
+#      ylab = "Average Glucose level",
+#      main = "Oversampled Data Distribution\n(Age v/s Glucose)")
+# legend(1, 275, legend=c("No Stroke", "Stroke"), col=c("black", "red"), lty=1:2, cex=0.5)
+# 
+# # --BMI v/s Glucose
+# plot(df$bmi, df$avg_glucose_level,
+#      col = df$stroke, pch=43, cex=1,
+#      xlab = "BMI",
+#      ylab = "Average Glucose level",
+#      main = "Oversampled Data Distribution\n(BMI v/s Glucose)")
+# legend(80, 275, legend=c("No Stroke", "Stroke"), col=c("black", "red"), lty=1:2, cex=0.5)
+# 
+# 
+# # --Plotting All 3 continuous Variables
+# color <- c("#E69F00", "#56B4E9")
+# shape <- c(42,43)
+# scatterplot3d(x = df$age, xlab = "Age",
+#               z = df$avg_glucose_level, zlab = "Avg. Glucose Level",
+#               y = df$bmi, ylab = "BMI",
+#               main = "Oversampled Stroke DataSet",
+#               pch = shape[as.numeric(df$stroke)], 
+#               color = color[as.numeric(df$stroke)],
+#               box = FALSE, angle = 20)
 
 #----Stratified Sampling(UnderSampling)----
 
@@ -131,13 +131,13 @@ scatterplot3d(x = df$age, xlab = "Age",
               z = df$avg_glucose_level, zlab = "Avg. Glucose Level",
               y = df$bmi, ylab = "BMI",
               main = "Undersampled Stroke DataSet",
-              pch = shape[as.numeric(df$stroke)], 
+              pch = shape[as.numeric(df$stroke)],
               color = color[as.numeric(df$stroke)],
               box = FALSE, angle = 20)
 
 
 
-#----Normalise Continuous Variables----
+# #----Normalise Continuous Variables----
 
 normalize <- function(x){
   return ((x - mean(x)) / sd(x))
@@ -159,7 +159,8 @@ test <- df[-tr,]
 # Recall_vals Will later be populated as a column vector containing recall
 # value for each 'k' nearest neighbour
 Recall_vals <- NULL
-kk <- seq(2,tr_size, by=50) # If Using SMOTE
+Acc_vals <- NULL
+# kk <- seq(2,tr_size, by=100) # If Using SMOTE
 kk <- seq(2,tr_size)        # If using UnderSampling
 
 # Trying out different neighbourhood size
@@ -170,7 +171,8 @@ for(i in kk)
   # 1. Confusion Matrix
   cm <- confusionMatrix(near$fitted, test$stroke, positive = "1")
   recall <- as.numeric(cm$byClass["Recall"])
-
+  acc <- as.numeric(cm$byClass["Balanced Accuracy"])
+  Acc_vals <- c(Acc_vals, acc)
   Recall_vals <- c(Recall_vals,recall)
   cat(i," -> ",recall,'\n')
 }
@@ -178,14 +180,19 @@ for(i in kk)
 best <- which.max(Recall_vals)
 best
 max(Recall_vals)
+Recall_vals[best]
+Acc_vals[best]
 
 # Plot of RMSE v/s neighborhood size
 plot(log(1/kk),Recall_vals,type="l",
      xlab="Complexity (log(1/k))",
      ylab="Recall",
-     main = "Plot of Recall v/s \nNeighbourhood size(Model Complexity)",
+     main = paste("Plot of Recall v/s Model Complexity\nBest Recall: ",
+                  round(max(Recall_vals), digits=2),
+                  "\nAccuracy: ",
+                  round(max(Acc_vals), digits=2)),
      col="blue",lwd=2,cex.lab=1.2)
-text(log(1/kk[1]),(Recall_vals[1]),paste("k=",kk[1]),col=2,cex=1.2)
+text(log(1/kk[1])-0.5,(Recall_vals[1]),paste("k=",kk[1]),col=2,cex=1.2)
 text(log(1/kk[best]),(Recall_vals[best]-0.1),paste("best k=",kk[best]),col=2,cex=1.2)
 text(log(1/kk[364])+0.4,(Recall_vals[364]+0.05),paste("k=",kk[364]),col=2,cex=1.2)
 
@@ -202,10 +209,13 @@ kcv = 10
 n0 = ceiling(n/kcv)
 
 out_Recall = matrix(0,kcv,tr_size)
+out_accuracy <- matrix(0,kcv,tr_size)
 used = NULL
 set = 1:n
 
-kk <- seq(2,tr_size)
+# kk <- seq(1,tr_size, by=100) # If Using SMOTE
+kk <- seq(1,tr_size)        # If using UnderSampling
+
 
 for(j in seq(1,kcv))
 {
@@ -225,9 +235,11 @@ for(j in seq(1,kcv))
     # 1. Confusion Matrix
     cm <- confusionMatrix(near$fitted, test_i$stroke, positive = "1")
     recall <- as.numeric(cm$byClass["Recall"])
+    acc <- as.numeric(cm$byClass["Balanced Accuracy"])
     
-    cat("(",j,")",i," -> ",recall,'\n')
+    #cat("(",j,")",i," -> ",recall,'\n')
     out_Recall[j,i] = recall
+    out_accuracy[j,i] <- acc
   }
   
   used = union(used,val)
@@ -237,17 +249,24 @@ for(j in seq(1,kcv))
 }
 
 mRecall = apply(out_Recall,2,mean)
+mAcc = apply(out_accuracy, 2, mean)
 
 best = which.max(mRecall)
 best
 max(mRecall)
+mRecall[best]
+mAcc[best]
 
-plot(log(1/(1:tr_size)),mRecall,
+plot(log(1/kk),mRecall,
      xlab="Complexity (log(1/k))",
      ylab="out-of-sample Recall",
      col=4,lwd=2,type="l",
      cex.lab=1.2,
-     main=paste("Knn- Age\n Best Recall: ",round(max(mRecall), digits=2)))
+     main=paste("Knn- Age\n Best Recall: ",
+                round(mRecall[best], digits=2),
+                "\n Accuracy: ",
+                round(mAcc[best], digits=2))
+     )
 
 text(log(1/best),mRecall[best]-0.1,paste("best k=",kk[best]),col=2,cex=1.2)
 text(log(1/2),mRecall[2],paste("k=",2),col=2,cex=1.2)
@@ -265,10 +284,12 @@ kcv = 10
 n0 = ceiling(n/kcv)
 
 out_Recall = matrix(0,kcv,tr_size)
+out_accuracy <- matrix(0,kcv,tr_size)
 used = NULL
 set = 1:n
 
-kk <- seq(2,tr_size)
+# kk <- seq(1,tr_size, by=100) # If Using SMOTE
+kk <- seq(1,tr_size)        # If using UnderSampling
 
 for(j in seq(1,kcv))
 {
@@ -288,7 +309,8 @@ for(j in seq(1,kcv))
     # 1. Confusion Matrix
     cm <- confusionMatrix(near$fitted, test_i$stroke, positive = "1")
     recall <- as.numeric(cm$byClass["Recall"])
-    
+    acc <- as.numeric(cm$byClass["Balanced Accuracy"])
+    out_accuracy[j,i] <- acc
     # cat("(",j,")",i," -> ",recall,'\n')
     out_Recall[j,i] = recall
   }
@@ -300,17 +322,24 @@ for(j in seq(1,kcv))
 }
 
 mRecall = apply(out_Recall,2,mean)
+mAcc = apply(out_accuracy, 2, mean)
 
 best = which.max(mRecall)
 best
 max(mRecall)
+mRecall[best]
+mAcc[best]
 
-plot(log(1/(1:tr_size)),mRecall,
+plot(log(1/kk),mRecall,
      xlab="Complexity (log(1/k))",
      ylab="out-of-sample Recall",
      col=4,lwd=2,type="l",
      cex.lab=1.2,
-     main=paste("Knn- Age+Avg.Glucose\n Best Recall: ",round(max(mRecall), digits=2)))
+     main=paste("Knn- Age+Avg.Glucose\n Best Recall: ",
+                round(mRecall[best], digits=2),
+                "\n Accuracy: ",
+                round(mAcc[best], digits=2))
+)
 
 text(log(1/best),mRecall[best]-0.1,paste("best k=",kk[best]),col=2,cex=1.2)
 text(log(1/2),mRecall[2],paste("k=",2),col=2,cex=1.2)
@@ -328,10 +357,13 @@ kcv = 10
 n0 = ceiling(n/kcv)
 
 out_Recall = matrix(0,kcv,tr_size)
+out_accuracy <- matrix(0,kcv,tr_size)
 used = NULL
 set = 1:n
 
-kk <- seq(2,tr_size)
+# kk <- seq(1,tr_size, by=100) # If Using SMOTE
+kk <- seq(1,tr_size)        # If using UnderSampling
+
 
 for(j in seq(1,kcv))
 {
@@ -351,6 +383,8 @@ for(j in seq(1,kcv))
     # 1. Confusion Matrix
     cm <- confusionMatrix(near$fitted, test_i$stroke, positive = "1")
     recall <- as.numeric(cm$byClass["Recall"])
+    acc <- as.numeric(cm$byClass["Balanced Accuracy"])
+    out_accuracy[j,i] <- acc
     
     # cat("(",j,")",i," -> ",recall,'\n')
     out_Recall[j,i] = recall
@@ -363,17 +397,24 @@ for(j in seq(1,kcv))
 }
 
 mRecall = apply(out_Recall,2,mean)
+mAcc = apply(out_accuracy, 2, mean)
 
 best = which.max(mRecall)
 best
 max(mRecall)
+mRecall[best]
+mAcc[best]
 
-plot(log(1/(1:tr_size)),mRecall,
+plot(log(1/kk),mRecall,
      xlab="Complexity (log(1/k))",
      ylab="out-of-sample Recall",
      col=4,lwd=2,type="l",
      cex.lab=1.2,
-     main=paste("Knn- Age+Avg.Glucose+BMI\n Best Recall: ",round(max(mRecall), digits=2)))
+     main=paste("Knn- Age+Avg.Glucose+BMI\n Best Recall: ",
+                round(mRecall[best], digits=2),
+                "\n Accuracy: ",
+                round(mAcc[best], digits=2))
+)
 
 text(log(1/best),mRecall[best]-0.1,paste("best k=",kk[best]),col=2,cex=1.2)
 text(log(1/2),mRecall[2],paste("k=",2),col=2,cex=1.2)
@@ -385,63 +426,74 @@ summary(near$fitted.values)
 
 
 #----KFold KNN (All Params)----
-kcv = 10
-
-# Divide into groups of size n0
-n0 = ceiling(n/kcv)
-
-out_Recall = matrix(0,kcv,tr_size)
-used = NULL
-set = 1:n
-
-kk <- seq(2,tr_size)
-
-for(j in seq(1,kcv))
-{
-  if(n0<length(set)){
-    val = sample(set,n0)
-  } else {
-    val=set
-  }
-  
-  train_i = df[-val,]
-  test_i = df[val,]
-  
-  for(i in kk)
-  {
-    near = kknn(stroke~.,train_i,test_i,k=i,kernel = "rectangular")
-    
-    # 1. Confusion Matrix
-    cm <- confusionMatrix(near$fitted, test_i$stroke, positive = "1")
-    recall <- as.numeric(cm$byClass["Recall"])
-    
-    # cat("(",j,")",i," -> ",recall,'\n')
-    out_Recall[j,i] = recall
-  }
-  
-  used = union(used,val)
-  set = (1:n)[-used]
-  
-  cat('------',j,'------\n')
-}
-
-mRecall = apply(out_Recall,2,mean)
-
-best = which.max(mRecall)
-best
-max(mRecall)
-
-plot(log(1/(1:tr_size)),mRecall,
-     xlab="Complexity (log(1/k))",
-     ylab="out-of-sample Recall",
-     col=4,lwd=2,type="l",
-     cex.lab=1.2,
-     main=paste("Knn- All Params\n Best Recall: ",round(max(mRecall), digits=2)))
-
-text(log(1/best),mRecall[best]-0.1,paste("best k=",kk[best]),col=2,cex=1.2)
-text(log(1/2),mRecall[2],paste("k=",2),col=2,cex=1.2)
-text(log(1/tr_size)+0.4,mRecall[tr_size],paste("k=",tr_size),col=2,cex=1.2)
-
-
-near <- kknn(stroke~.,train,discarded_negs,k=kk[best],kernel = "rectangular")
-summary(near$fitted.values)
+# kcv = 10
+# 
+# # Divide into groups of size n0
+# n0 = ceiling(n/kcv)
+# 
+# out_Recall = matrix(0,kcv,tr_size)
+# out_accuracy <- matrix(0,kcv,tr_size)
+# used = NULL
+# set = 1:n
+# 
+# # kk <- seq(1,tr_size, by=100) # If Using SMOTE
+# kk <- seq(1,tr_size)        # If using UnderSampling
+# 
+# 
+# for(j in seq(1,kcv))
+# {
+#   if(n0<length(set)){
+#     val = sample(set,n0)
+#   } else {
+#     val=set
+#   }
+#   
+#   train_i = df[-val,]
+#   test_i = df[val,]
+#   
+#   for(i in kk)
+#   {
+#     near = kknn(stroke~.,train_i,test_i,k=i,kernel = "rectangular")
+#     
+#     # 1. Confusion Matrix
+#     cm <- confusionMatrix(near$fitted, test_i$stroke, positive = "1")
+#     recall <- as.numeric(cm$byClass["Recall"])
+#     acc <- as.numeric(cm$byClass["Balanced Accuracy"])
+#     out_accuracy[j,i] <- acc
+#     # cat("(",j,")",i," -> ",recall,'\n')
+#     out_Recall[j,i] = recall
+#   }
+#   
+#   used = union(used,val)
+#   set = (1:n)[-used]
+#   
+#   cat('------',j,'------\n')
+# }
+# 
+# mRecall = apply(out_Recall,2,mean)
+# mAcc = apply(out_accuracy, 2, mean)
+# 
+# best = which.max(mRecall)
+# best
+# max(mRecall)
+# mRecall[best]
+# mAcc[best]
+# 
+# plot(log(1/kk),mRecall,
+#      xlab="Complexity (log(1/k))",
+#      ylab="out-of-sample Recall",
+#      col=4,lwd=2,type="l",
+#      cex.lab=1.2,
+#      main=paste("Knn- All Params\n Best Recall: ",
+#                 round(mRecall[best], digits=2),
+#                 "\n Accuracy: ",
+#                 round(mAcc[best], digits=2))
+# )
+# 
+# text(log(1/best),mRecall[best]-0.1,paste("best k=",kk[best]),col=2,cex=1.2)
+# text(log(1/2),mRecall[2],paste("k=",2),col=2,cex=1.2)
+# text(log(1/tr_size)+0.4,mRecall[tr_size],paste("k=",tr_size),col=2,cex=1.2)
+# 
+# 
+# near <- kknn(stroke~.,train,discarded_negs,k=kk[best],kernel = "rectangular")
+# summary(near$fitted.values)
