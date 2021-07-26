94a4c72f31b693ecc63cd2487765c8849788d269
CaptainNemo <rohitashwachaks@gmail.com>
2021-07-26 03:22:25 -0500

    Knn Codes + Plots

--- knn.R
+++ knn.R
@@ -9,6 +9,10 @@ library(tidyr)
 #install.packages('caret', dependencies=TRUE)
 library(caret)
 
+# install.packages("scatterplot3d") # Install
+library("scatterplot3d")
+
+
 set.seed(0)
 
 #----Data Prep-----
@@ -58,12 +62,37 @@ discarded_negs = data[-(sample(1:dim(neg_samples)[1], dim(pos_samples)[1]*1.5)),
 df <- rbind(pos_samples, undersampled_negs)
 n <- dim(df)[1]
 
+plot(col = df$stroke, pch=43, cex=1,
+     main = "Sample class distribution",
+     df %>% select(age, bmi, avg_glucose_level))
 
-plot(df %>% select(age, bmi, avg_glucose_level, hypertension,
-                  gender, heart_disease, ever_married))
+# --Age v/s Glucose
+plot(df$age, df$avg_glucose_level,
+     col = df$stroke, pch=43, cex=1,
+     xlab = "Age",
+     ylab = "Average Glucose level",
+     main = "Undersampled Data Distribution\n(Age v/s Glucose)")
+legend(1, 275, legend=c("No Stroke", "Stroke"), col=c("black", "red"), lty=1:2, cex=0.8)
 
-plot(df$age, df$avg_glucose_level, col = df$stroke, pch=19, cex=1)
-## Add legends
+# --BMI v/s Glucose
+plot(df$bmi, df$avg_glucose_level,
+     col = df$stroke, pch=43, cex=1,
+     xlab = "BMI",
+     ylab = "Average Glucose level",
+     main = "Undersampled Data Distribution\n(BMI v/s Glucose)")
+legend(10, 275, legend=c("No Stroke", "Stroke"), col=c("black", "red"), lty=1:2, cex=0.5)
+
+
+# --Plotting All 3 continuous Variables
+color <- c("#E69F00", "#56B4E9")
+shape <- c(42,43)
+scatterplot3d(x = df$age, xlab = "Age",
+              z = df$avg_glucose_level, zlab = "Avg. Glucose Level",
+              y = df$bmi, ylab = "BMI",
+              main = "Undersampled Stroke DataSet",
+              pch = shape[as.numeric(df$stroke)], 
+              color = color[as.numeric(df$stroke)],
+              box = FALSE, angle = 20)
 
 #----Neighbourhood Size----
 
@@ -79,56 +108,167 @@ test <- df[-tr,]
 # value for each 'k' nearest neighbour
 Recall_vals <- NULL
 kk <- seq(2,tr_size)
-#i <- 10
 
 # Trying out different neighbourhood size
 for(i in kk)
 {
-  # stroke~ means predict stroke using everything
   near <- kknn(stroke~age+avg_glucose_level+bmi,train,test,k=i,kernel = "rectangular")
   
   # 1. Confusion Matrix
   cm <- confusionMatrix(near$fitted, test$stroke, positive = "1")
-
-  # Precision and Recall of Preds.
-  # precision <- as.numeric(cm$byClass["Precision"])
   recall <- as.numeric(cm$byClass["Recall"])
-  # F1 <- as.numeric(cm$byClass["F1"])
-  
-  # append aux to Recall_vals
+
   Recall_vals <- c(Recall_vals,recall)
-  
   cat(i," -> ",recall,'\n')
 }
-
-# Plot stroke v/s age
-# Add fitted line (K-nearest neighbour prediction for each point)
-# near <- kknn(stroke~age,train,test,k=i,kernel = "rectangular")
-# plot(train$age, train$stroke,main=paste("k=",i),pch=19,cex=0.8,col="darkgray")
-# lines(test[,1],near$fitted,col=2,lwd=2)
 
 best <- which.max(Recall_vals)
 best
 max(Recall_vals)
 
 # Plot of RMSE v/s neighborhood size
-plot(log(1/kk),Recall_vals,type="b",
+plot(log(1/kk),Recall_vals,type="l",
      xlab="Complexity (log(1/k))",
      ylab="Recall",
-     main = "Plot of Recall v/s Neighbourhood size(Model Complexity)",
+     main = "Plot of Recall v/s \nNeighbourhood size(Model Complexity)",
      col="blue",lwd=2,cex.lab=1.2)
-text(log(1/kk[1]),(Recall_vals[1])+0.3,paste("k=",kk[1]),col=2,cex=1.2)
-text(log(1/kk[best])+0.4,(Recall_vals[best]),paste("best k=",kk[best]),col=2,cex=1.2)
-text(log(1/kk[364])+0.4,(Recall_vals[364]),paste("k=",kk[364]),col=2,cex=1.2)
+text(log(1/kk[1]),(Recall_vals[1]),paste("k=",kk[1]),col=2,cex=1.2)
+text(log(1/kk[best]),(Recall_vals[best]-0.1),paste("best k=",kk[best]),col=2,cex=1.2)
+text(log(1/kk[364])+0.4,(Recall_vals[364]+0.05),paste("k=",kk[364]),col=2,cex=1.2)
 
 # Testing against discarded
 
 near <- kknn(stroke~age+avg_glucose_level+bmi,train,discarded_negs,k=kk[best],kernel = "rectangular")
-cm <- confusionMatrix(near$fitted, discarded_negs$stroke, positive = "1")
 summary(near$fitted.values)
 
 
-#----KFold KNN----
+#----KFold KNN (Age)----
+kcv = 10
+
+# Divide into groups of size n0
+n0 = ceiling(n/kcv)
+
+out_Recall = matrix(0,kcv,tr_size)
+used = NULL
+set = 1:n
+
+kk <- seq(2,tr_size)
+
+for(j in seq(1,kcv))
+{
+  if(n0<length(set)){
+    val = sample(set,n0)
+  } else {
+    val=set
+  }
+  
+  train_i = df[-val,]
+  test_i = df[val,]
+  
+  for(i in kk)
+  {
+    near = kknn(stroke~age,train_i,test_i,k=i,kernel = "rectangular")
+    
+    # 1. Confusion Matrix
+    cm <- confusionMatrix(near$fitted, test_i$stroke, positive = "1")
+    recall <- as.numeric(cm$byClass["Recall"])
+    
+    cat("(",j,")",i," -> ",recall,'\n')
+    out_Recall[j,i] = recall
+  }
+  
+  used = union(used,val)
+  set = (1:n)[-used]
+  
+  cat('------',j,'------\n')
+}
+
+mRecall = apply(out_Recall,2,mean)
+
+best = which.max(mRecall)
+best
+max(mRecall)
+
+plot(log(1/(1:tr_size)),mRecall,
+     xlab="Complexity (log(1/k))",
+     ylab="out-of-sample Recall",
+     col=4,lwd=2,type="l",
+     cex.lab=1.2,
+     main=paste("Knn- Age\n Best Recall: ",round(max(mRecall), digits=2)))
+
+text(log(1/best),mRecall[best]-0.1,paste("best k=",kk[best]),col=2,cex=1.2)
+text(log(1/2),mRecall[2],paste("k=",2),col=2,cex=1.2)
+text(log(1/tr_size)+0.4,mRecall[tr_size],paste("k=",tr_size),col=2,cex=1.2)
+
+
+near <- kknn(stroke~age,train,discarded_negs,k=kk[best],kernel = "rectangular")
+summary(near$fitted.values)
+
+
+#----KFold KNN (Age + Avg. Glucose)----
+kcv = 10
+
+# Divide into groups of size n0
+n0 = ceiling(n/kcv)
+
+out_Recall = matrix(0,kcv,tr_size)
+used = NULL
+set = 1:n
+
+kk <- seq(2,tr_size)
+
+for(j in seq(1,kcv))
+{
+  if(n0<length(set)){
+    val = sample(set,n0)
+  } else {
+    val=set
+  }
+  
+  train_i = df[-val,]
+  test_i = df[val,]
+  
+  for(i in kk)
+  {
+    near = kknn(stroke~age+avg_glucose_level,train_i,test_i,k=i,kernel = "rectangular")
+    
+    # 1. Confusion Matrix
+    cm <- confusionMatrix(near$fitted, test_i$stroke, positive = "1")
+    recall <- as.numeric(cm$byClass["Recall"])
+    
+    # cat("(",j,")",i," -> ",recall,'\n')
+    out_Recall[j,i] = recall
+  }
+  
+  used = union(used,val)
+  set = (1:n)[-used]
+  
+  cat('------',j,'------\n')
+}
+
+mRecall = apply(out_Recall,2,mean)
+
+best = which.max(mRecall)
+best
+max(mRecall)
+
+plot(log(1/(1:tr_size)),mRecall,
+     xlab="Complexity (log(1/k))",
+     ylab="out-of-sample Recall",
+     col=4,lwd=2,type="l",
+     cex.lab=1.2,
+     main=paste("Knn- Age+Avg.Glucose\n Best Recall: ",round(max(mRecall), digits=2)))
+
+text(log(1/best),mRecall[best]-0.1,paste("best k=",kk[best]),col=2,cex=1.2)
+text(log(1/2),mRecall[2],paste("k=",2),col=2,cex=1.2)
+text(log(1/tr_size)+0.4,mRecall[tr_size],paste("k=",tr_size),col=2,cex=1.2)
+
+
+near <- kknn(stroke~age+avg_glucose_level,train,discarded_negs,k=kk[best],kernel = "rectangular")
+summary(near$fitted.values)
+
+
+#----KFold KNN (Age + Avg. Glucose + BMI)----
 kcv = 10
 
 # Divide into groups of size n0
@@ -157,14 +297,9 @@ for(j in seq(1,kcv))
     
     # 1. Confusion Matrix
     cm <- confusionMatrix(near$fitted, test_i$stroke, positive = "1")
-    
-    # Precision and Recall of Preds.
-    # precision <- as.numeric(cm$byClass["Precision"])
     recall <- as.numeric(cm$byClass["Recall"])
-    # F1 <- as.numeric(cm$byClass["F1"])
     
-    cat("(",j,")",i," -> ",recall,'\n')
-    
+    # cat("(",j,")",i," -> ",recall,'\n')
     out_Recall[j,i] = recall
   }
   
@@ -176,13 +311,18 @@ for(j in seq(1,kcv))
 
 mRecall = apply(out_Recall,2,mean)
 
-plot(log(1/(1:tr_size)),mRecall,xlab="Complexity (log(1/k))",ylab="out-of-sample Recall",col=4,lwd=2,type="l",cex.lab=1.2,main=paste("kfold(",kcv,")"))
-
 best = which.max(mRecall)
 best
 max(mRecall)
 
-text(log(1/best),mRecall[best]+0.1,paste("k=",kk[best]),col=2,cex=1.2)
+plot(log(1/(1:tr_size)),mRecall,
+     xlab="Complexity (log(1/k))",
+     ylab="out-of-sample Recall",
+     col=4,lwd=2,type="l",
+     cex.lab=1.2,
+     main=paste("Knn- Age+Avg.Glucose+BMI\n Best Recall: ",round(max(mRecall), digits=2)))
+
+text(log(1/best),mRecall[best]-0.1,paste("best k=",kk[best]),col=2,cex=1.2)
 text(log(1/2),mRecall[2],paste("k=",2),col=2,cex=1.2)
 text(log(1/tr_size)+0.4,mRecall[tr_size],paste("k=",tr_size),col=2,cex=1.2)
 
@@ -191,24 +331,64 @@ near <- kknn(stroke~age+avg_glucose_level+bmi,train,discarded_negs,k=kk[best],ke
 summary(near$fitted.values)
 
 
-#----ROUGH----
-# 
-# cost <- function(pred, y){
-#   shift <- length(pred)/2
-#   cst <- 0
-#   for (i in 1:length(y)){
-#     
-#     #cat(i,"->", as.character(y[i])," : ",
-#       #  pred[c(5,(length(pred)/2)+5)],"\n")
-#     
-#     if(as.character(y[i]) == "1"){
-#       cst <- cst -log(pred[shift+i])
-#     }
-#     else{
-#       cst <- cst - log(pred[i])
-#     }
-#   }
-#   return (cst)
-# }
-# 
-# cost(near$prob, test$stroke)
+#----KFold KNN (All Params)----
+kcv = 10
+
+# Divide into groups of size n0
+n0 = ceiling(n/kcv)
+
+out_Recall = matrix(0,kcv,tr_size)
+used = NULL
+set = 1:n
+
+kk <- seq(2,tr_size)
+
+for(j in seq(1,kcv))
+{
+  if(n0<length(set)){
+    val = sample(set,n0)
+  } else {
+    val=set
+  }
+  
+  train_i = df[-val,]
+  test_i = df[val,]
+  
+  for(i in kk)
+  {
+    near = kknn(stroke~.,train_i,test_i,k=i,kernel = "rectangular")
+    
+    # 1. Confusion Matrix
+    cm <- confusionMatrix(near$fitted, test_i$stroke, positive = "1")
+    recall <- as.numeric(cm$byClass["Recall"])
+    
+    # cat("(",j,")",i," -> ",recall,'\n')
+    out_Recall[j,i] = recall
+  }
+  
+  used = union(used,val)
+  set = (1:n)[-used]
+  
+  cat('------',j,'------\n')
+}
+
+mRecall = apply(out_Recall,2,mean)
+
+best = which.max(mRecall)
+best
+max(mRecall)
+
+plot(log(1/(1:tr_size)),mRecall,
+     xlab="Complexity (log(1/k))",
+     ylab="out-of-sample Recall",
+     col=4,lwd=2,type="l",
+     cex.lab=1.2,
+     main=paste("Knn- All Params\n Best Recall: ",round(max(mRecall), digits=2)))
+
+text(log(1/best),mRecall[best]-0.1,paste("best k=",kk[best]),col=2,cex=1.2)
+text(log(1/2),mRecall[2],paste("k=",2),col=2,cex=1.2)
+text(log(1/tr_size)+0.4,mRecall[tr_size],paste("k=",tr_size),col=2,cex=1.2)
+
+
+near <- kknn(stroke~.,train,discarded_negs,k=kk[best],kernel = "rectangular")
+summary(near$fitted.values)
